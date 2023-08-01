b7d05b9e56aaa0615536bb912c1d778d663f7a53
Kevin Rivera <114261500+keriz-rh@users.noreply.github.com>
2023-08-01 01:00:26 -0600

    Update MiConversor.java

--- ConversorDeMonedas/src/MiConversor.java
+++ ConversorDeMonedas/src/MiConversor.java
@@ -1,5 +1,4 @@
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
@@ -19,6 +18,7 @@ public class MiConversor {
 	private JLabel lbl;
 	private JTextField txt;
 	
+	// Define una enumeración para representar diferentes monedas
 	public enum Moneda {
 		dolar_pesos,
 		dolar_euro,
@@ -32,6 +32,7 @@ public class MiConversor {
 		WonSurCoreano_dolar
 	}
 	
+	// Tasas de cambio para diferentes monedas
 	public double peso = 16.75;
 	public double euro = 0.91;
 	public double libraEsterlinas = 0.78;
@@ -41,7 +42,7 @@ public class MiConversor {
 	public double valorInput = 0.00;
 	
 	/**
-	 * Launch the application.
+	 * Ejecuta la aplicación.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -57,14 +58,14 @@ public class MiConversor {
 	}
 
 	/**
-	 * Create the application.
+	 * Crea la aplicación.
 	 */
 	public MiConversor() {
 		initialize();
 	}
 
 	/**
-	 * Initialize the contents of the frame.
+	 * Inicializa el contenido del marco (frame).
 	 */
 	private void initialize() {
 		frame = new JFrame();
@@ -82,15 +83,13 @@ public class MiConversor {
 		cmb.setBounds(34, 95, 206, 24);
 		frame.getContentPane().add(cmb);
 		
-		// button event
+		// Agrega un botón y establece su evento de clic
 		btn = new JButton("Convertir.");
 		btn.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				Convertir();
+				Convertir(); // Llama al método de conversión cuando se hace clic en el botón.
 			}
-			
-			
 		});
 		btn.setBounds(257, 97, 147, 21);
 		frame.getContentPane().add(btn);
@@ -100,63 +99,69 @@ public class MiConversor {
 		frame.getContentPane().add(lbl);
 	}
 	
+	// Método de conversión que realiza la conversión de monedas
 	public void Convertir() {
-		if (Validar(txt.getText()) ) {
-			Moneda moneda = (Moneda)cmb.getSelectedItem();
+		if (Validar(txt.getText()) ) { // Valida si la entrada es un número válido
+			Moneda moneda = (Moneda) cmb.getSelectedItem(); // Obtiene la moneda seleccionada del combo box.
 			
+			// Realiza la conversión según la moneda seleccionada
 			switch (moneda) {
-			case dolar_pesos: 
-				DolarAMoneda(peso);
-				break;
-			case dolar_euro: 
-				DolarAMoneda(euro);
-				break;
-			case dolar_libraEsterlinas:
-				DolarAMoneda(libraEsterlinas);
-				break;
-			case dolar_YenJapones:
-				DolarAMoneda(YenJapones);
-				break;
-			case dolar_WonSurCoreano:
-				DolarAMoneda(WonSurCoreano);
-				break;
-			case pesos_dolar: 
-				MonedaADolar(peso);
-				break;
-			case euro_dolar: 
-				MonedaADolar(euro);
-				break;
-			case libraEsterlinas_dolar: 
-				MonedaADolar(libraEsterlinas);
-				break;
-			case YenJapones_dolar:
-				MonedaADolar(YenJapones);
-				break;
-			case WonSurCoreano_dolar:
-				MonedaADolar(WonSurCoreano);
-			
-			default:
-				throw new IllegalArgumentException("Unexpected value: " + moneda);
-				
+				case dolar_pesos: 
+					DolarAMoneda(peso);
+					break;
+				case dolar_euro: 
+					DolarAMoneda(euro);
+					break;
+				case dolar_libraEsterlinas:
+					DolarAMoneda(libraEsterlinas);
+					break;
+				case dolar_YenJapones:
+					DolarAMoneda(YenJapones);
+					break;
+				case dolar_WonSurCoreano:
+					DolarAMoneda(WonSurCoreano);
+					break;
+				case pesos_dolar: 
+					MonedaADolar(peso);
+					break;
+				case euro_dolar: 
+					MonedaADolar(euro);
+					break;
+				case libraEsterlinas_dolar: 
+					MonedaADolar(libraEsterlinas);
+					break;
+				case YenJapones_dolar:
+					MonedaADolar(YenJapones);
+					break;
+				case WonSurCoreano_dolar:
+					MonedaADolar(WonSurCoreano);
+					break;
+				default:
+					throw new IllegalArgumentException("Valor inesperado: " + moneda);
+			}
 		}
-}
 	}
 	
+	// Convierte de USD a la moneda seleccionada
 	public void DolarAMoneda(double moneda) {
-		double res = valorInput * moneda;
-		lbl.setText(Redondear(res));
-	}
-	public void MonedaADolar(double moneda) {
-		double res = valorInput / moneda;
-		lbl.setText(Redondear(res));
+		double res = valorInput * moneda; // Realiza la conversión
+		lbl.setText(Redondear(res)); // Muestra el resultado con el formato apropiado
 	}
 	
+	// Convierte de la moneda seleccionada a USD
+	public void MonedaADolar(double moneda) {
+		double res = valorInput / moneda; // Realiza la conversión
+		lbl.setText(Redondear(res)); // Muestra el resultado con el formato apropiado
+	}
+	
+	// Método de utilidad para redondear un valor double a dos decimales
 	public String Redondear(double valor) {
 		DecimalFormat df = new DecimalFormat("0.00");
 		df.setRoundingMode(RoundingMode.HALF_UP);
 		return df.format(valor);
 	}
 	
+	// Método de utilidad para validar si la entrada es un número válido y positivo
 	public boolean Validar(String texto) {
 		try {
 			double x = Double.parseDouble(texto);
@@ -165,9 +170,9 @@ public class MiConversor {
 				return true;
 			}
 		} catch (NumberFormatException e) {
-			lbl.setText("¡Solamente números!");
+			lbl.setText("¡Solamente números!"); // Muestra un mensaje de error si la entrada no es un número válido
 			return false;
 		}
+		return false;
 	}
-	
 }
